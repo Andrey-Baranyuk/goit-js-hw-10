1f0c84af88c836dac9192d1e5519f1fbb70e6245
Andrey-Baranyuk <andreybaranyuk86@gmail.com>
2022-02-11 16:12:17 +0200

    create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+import { debounce } from 'lodash';
+import { Notify } from 'notiflix';
+
+import RenderService from './render.js';
+import Country from './api/fetchCountries.js';
+
+const DEBOUNCE_DELAY = 300
+
+const inputField = document.querySelector('#search-box')
+
+const countryAPI = new Country()
+const renderService = new RenderService()
+
+inputField.addEventListener('input', debounce(() => {
+  
+  let inputValue = inputField.value.trim()
+  countryAPI.query = inputValue
+  const minLetter = inputValue.length
+
+  if (minLetter >= 1) {
+    countryAPI.fetchCountry()
+      .then(countrysArray => {
+
+          if (countrysArray.length >= 10) {
+            
+            return Notify.info('Too many matches found. Please enter a more specific name')
+              
+        } else if (countrysArray.length === 1) {
+          console.log(countrysArray)
+
+          renderService.renderCountryInfo(countrysArray)
+
+        } else if (countrysArray.length < 10 && countrysArray.length >= 2) {
+
+          renderService.renderCountryList(countrysArray)
+        } else {
+          return Notify.failure('Oops, there is no country with that name')
+        }
+      })
+      .catch(error => console.log(error))
+
+  } else if (minLetter === 0) {
+    renderService.clearList()
+  }
+    }, DEBOUNCE_DELAY))
